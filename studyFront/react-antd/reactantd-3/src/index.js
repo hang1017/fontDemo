90a11e94cd0a43d4b2baf28b69eb3fcf4e7e0905
chenshuhang <437132124@qq.com>
2019-04-08 23:34:53 +0800

    流浪猫狗--19.4.8晚

--- studyFront/react-antd/reactantd-3/src/index.js
+++ studyFront/react-antd/reactantd-3/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Button,Layout,Typography,Carousel,Card } from 'antd';
+import { Button,Layout,Typography,Carousel,Card,Row, Col,Modal } from 'antd';
 
 const {Text} = Typography;
 const {Header, Content,} = Layout;
@@ -9,27 +9,44 @@ const {Header, Content,} = Layout;
 
 class DogCard extends React.Component{
 
-    constructor(){
-        super();
-    }
-
-    mouserMove(){
-        // alert("");
+    constructor(props){
+        super(props);
     }
 
     render(){
         return(
-            <div className="dc_main"  hoverable onMouseMove={this.mouserMove}>
-                {/* <Card className="dc_card"
+            <div className="dc_main">
+                <Card className="dc_card"
                     hoverable
-                >  */}
+                > 
+                <div className="dc_all">
                     <div>
                         <img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
                     </div>
                     <div className="dc_right">
-                        
+                        <Row>
+                            <Col span={6} offset={2}>Name:</Col>
+                            <Col span={16} >{this.props.detail.name}</Col>
+                        </Row>
+                        <Row>
+                            <Col span={6} offset={2}>Age:</Col>
+                            <Col span={16} >{this.props.detail.age}</Col>
+                        </Row>
+                        <Row>
+                            <Col span={6} offset={2}>phone:</Col>
+                            <Col span={16} >{this.props.detail.phone}</Col>
+                        </Row>
+                        <Row>
+                            <Col span={6} offset={2}>address:</Col>
+                            <Col span={16} >{this.props.detail.address}</Col>
+                        </Row>
+                        <Row>
+                            <Col span={6} offset={2}>detail:</Col>
+                            <Col span={16} >{this.props.detail.detail}</Col>
+                        </Row>
                     </div>
-                {/* </Card> */}
+                </div>
+                </Card>
                 
             </div>
         )
@@ -38,7 +55,41 @@ class DogCard extends React.Component{
 
 class FindDog extends React.Component{
 
+    constructor(){
+        super();
+        this.state = {
+            details:[{
+                "name":"hang1",
+                "age":"15",
+                "detail":"黑黑的",
+                "address":"中海寰宇天下",
+                "phone":"1234567"
+            },{
+                "name":"hang2",
+                "age":"15",
+                "detail":"白白的",
+                "address":"中海寰宇天下",
+                "phone":"1234567890"
+            },{
+                "name":"hang3",
+                "age":"15",
+                "detail":"白白的",
+                "address":"中海寰宇天下",
+                "phone":"1234567890"
+            },{
+                "name":"hang4",
+                "age":"15",
+                "detail":"白白的",
+                "address":"中海寰宇天下",
+                "phone":"1234567890"
+            }]
+        }
+    }
+
     render(){
+
+        const {details} = this.state;
+
         return(
             <div>
                 <Layout>
@@ -47,6 +98,7 @@ class FindDog extends React.Component{
                             <Text className="header_text">流浪猫狗救助站</Text>
                         </div>
                         <div className="header_button_div">
+                            <Text className="header_button_text">航帅帅</Text>
                             <Button>个人中心</Button>
                             <Button>登录</Button>
                         </div>
@@ -61,8 +113,12 @@ class FindDog extends React.Component{
                             </Carousel>
                         </div>
                         <div className="content_card_div">
-                            <DogCard/>
-                            <DogCard/>
+                            {
+                                details.map((item, index) => (
+                                    <DogCard detail={item}/>
+                                ))
+                            }
+                            
                         </div>
                     </Content>
 
