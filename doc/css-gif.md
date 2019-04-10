84301fed61e4e2a856e50b0d2e15fdd7287be213
chenshuhang <437132124@qq.com>
2019-04-10 19:13:36 +0800

    css-gif---文档+代码----2019.04.10

---
+++ doc/css-gif.md
@@ -0,0 +1,168 @@
+# 通过动图来展示 CSS 的相关知识点
+
+## 一、负边距
+
+左右边距表现效果不一致
+
+左、上为负：左、上移
+
+右、下为负：右、下移,将后面的元素向后或者向下推
+
+## 二、形状的外在表现
+
+弄不好
+
+## 三、flex 不为人知的特性
+
+1、父控件下的：`margin:auto`
+
+2、`flex-grow<1`:只能按比例分配部分剩余空间，而不是全部
+
+
+## 四、input 宽度
+
+并不是给元素设置 `display:block` 就会自动填充父元素宽度，input 就是个例外。其默认宽度取决于 `size` 特性
+
+## 五、position 定位特效
+
+同级元素中，要想有谁显示在前的样式，要给每个元素都附上 `position:relative` 
+
+然后通过 `z-index` 调整谁先谁后
+
+## 六、粘性定位
+
+对要使用粘性定位的元素上加上 `position:sticky` 属性
+
+位置的控制：`top`、`button`  单位：`vh`
+
+## 七、相邻兄弟选择器
+
+不会说直接看代码
+
+```html
+<div className="o7">
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+    </ul>
+</div>
+```
+```css
+.o7>ul{
+    list-style: none;
+    text-align: center;
+    padding-left: 0;
+    /* padding-top: 0; */
+}
+
+.o7>ul>li+li{
+    border-top: 1px dashed red;
+}
+```
+## 八、css 绘制三角形
+
+```css
+.o8-1{
+    border: 100px solid;
+    border-color: red yellow transparent gray;
+}
+```
+
+可以直接看 css
+
+边框的的宽度设置的大一些即可
+
+## 九、display 的 table 布局
+
+`display:table` 可以实现多列等高布局
+
+父元素定义为：`display:table` 
+
+子元素定义为：`display:table-cell`
+
+如果有需要缩进的话在父元素补上： `border-spacing: 30px 10px;`
+
+## 十、动画效果
+
+可以参考 freeCodeCamp :CSS->23
+
+使用动画:
+
+```css
+animation: move 2s linear infinite;
+```
+
+如果需要改变运用方向的话，可以使用 `animation-direction:`
+
+并且需要使用 `@keyframes` 来设置动画效果,下面为参考代码：
+
+```css
+@keyframes move{
+    0%{
+        background-color: red;
+    }
+    100%{
+        transform: translate(400%,0);
+    }
+}
+```
+
+## 十一、渐变色
+
+直接参考 freeCodeCamp CSS=>21
+
+## 十二、隐藏文字的两种方法：
+
+1、设置文字大小：
+
+```css
+font-size:0px;
+```
+
+2、设置偏移：
+
+```css
+text-indent:-1000px;
+```
+
+## 十三、角度渐变
+
+```css
+background: conic-gradient(red 0 30%,green 40% 70%,blue 70% 100%);
+```
+
+## 十四、background-repeat
+
+两个新属性：round、space
+
+前者：凑个整
+
+后者：留点缝
+
+## 十五、指定背景如何附在容器上
+
+`background-attachment:`
+
+注意其属性 `local` 和 `fixed`的使用
+
+## 十六、 outline
+
+用 `outline` 来描边，不占地方，也可以在里面
+
+代码一看便知：
+
+```css
+outline: 5px solid green;
+outline-offset: -10px;
+```
+
+
+
+
+
+
+
+
---
+++ studyFront/css-gif/src/index.js
@@ -0,0 +1,88 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+class CssGif extends React.Component{
+    render(){
+        return(
+            <div>
+                <div className = "o1">
+                    <div className="o1-1"></div>
+                    <div className="o1-2"></div>
+                </div>
+                <div className = "o2">
+                    <div className="o2-1"></div>
+                    <div className="o2-2">aaaaaaaaaaaaaaaaaa</div>
+                </div>
+                <div className="o3">
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                    <div>4</div>
+                </div>
+                {/* 定位特效 */}
+                <div className="o4">
+                    <input size="30"/><br/>
+                    <span className="o4-1">1</span>
+                </div>
+                <div className="o5">
+                    <div className="o5-1">
+                        <div className="o5-4"></div>
+                    </div>
+                    <div className="o5-2"></div>
+                    <div className="o5-3"></div>
+                </div>
+                {/* 粘性特性 */}
+                {/* <div className="o6">
+                    <div className="o6-1"></div>
+                    <div className="o6-2">111</div>
+                    <div className="o6-3"></div>
+                </div> */}
+                {/* 相邻兄弟元素 */}
+                <div className="o7">
+                    <ul>
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                        <li>4</li>
+                        <li>5</li>
+                    </ul>
+                </div>
+                {/* css绘制三角形 */}
+                <div className="o8">
+                    <div className="o8-1"></div>
+                </div>
+                {/* flex table布局 */}
+                <div className="o9">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                {/* 动画方向 */}
+                <div className="o10">
+                    <div className="o10-1"></div>
+                    <div className="o10-2"></div>
+                    <div className="o10-3"></div>
+                    <div className="o10-4"></div>
+                </div>
+                
+                {/* 角向渐变 */}
+                <div className="o11"></div>
+                {/* 图片 repeat */}
+                {/* <div className="o12">
+                    <img src="../../studyImg/biaoqing.png" alt="img" />
+                </div> */}
+                
+                <div className="o13">
+                    
+                </div>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <CssGif/>,
+    document.getElementById('root')
+)
