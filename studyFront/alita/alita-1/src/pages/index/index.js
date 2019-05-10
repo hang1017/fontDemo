ad354155c412f3b2e0dc61b0447fa6cd3bcb58c2
chenshuhang <437132124@qq.com>
2019-05-10 09:58:13 +0800

    alitademo-2019.05.10

---
+++ studyFront/alita/alita-1/src/pages/index/index.js
@@ -0,0 +1,32 @@
+import { connect } from 'dva';
+import React, { Component } from 'react';
+import { Link } from 'umi';
+import { Button } from 'antd';
+import { router } from 'umi';
+
+import styles from './index.less';
+
+@connect(({ index }) => ({ index }))
+class Page extends Component{
+  state = {};
+
+  render() {
+    const {
+      index: { name,items },
+    } = this.props;
+    console.log(items);
+
+    const gotoList = () => {
+      router.push('/list');
+    }
+
+    return <div className={styles.userCenter}>
+      <Link to="/home">跳转回 home 页面</Link><br/>
+      <Button onClick={gotoList}>跳转回 list 页面</Button>
+      Hello {name}
+    </div>;
+
+  }
+}
+
+export default Page;
